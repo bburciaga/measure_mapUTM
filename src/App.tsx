06fa642a91187345560ebdc94fdca567251a4033
Brian Burciaga <52196460+bburciaga@users.noreply.github.com>
2021-07-28 13:29:03 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import logo from './logo.svg';
+import Measure from './MeasureHelpers/Measure';
+import './App.css';
+
+function App() {
+  const [map, setMap] = useState<any>(null);
+
+  return (
+    <div className="App">
+      <MapContainer id="mapid" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} whenCreated={setMap}>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'flex-end',
+          flexFlow: 'column wrap',
+          height: '70vh'
+        }}>
+          { map ? <Measure map={map} /> : null }
+        </div>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MeasureHelpers/Measure.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { IconButton, makeStyles } from '@material-ui/core';
+import { Marker, Popup, useMapEvents } from 'react-leaflet';
+import L from 'leaflet';
+import proj4 from 'proj4';
+import ruler from './ruler.png';
+import dotMarker from './dotMarker.png';
+import RadioButtonCheckedIcon from '@material-ui/icons/RadioButtonChecked';
+
+const useStyles = makeStyles((theme) => ({
+  image: {
+    height: 24,
+    width: 24
+  },
+  rulerButton: {
+    margin: '5px',
+    background: 'white',
+    zIndex: 1500,
+    height: '48px', width: '48px',
+    borderRadius: '4px'
+  }
+}));
+
+const utm_zone = (longitude: any, latitude: any) => {
+  let utmZone = ((Math.floor((longitude + 180) / 6) % 60) + 1).toString(); //getting utm zone
+        proj4.defs([
+            ['EPSG:4326', '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'],
+            ['EPSG:AUTO', `+proj=utm +zone=${utmZone} +datum=WGS84 +units=m +no_defs`]
+        ]);
+        const en_m = proj4('EPSG:4326', 'EPSG:AUTO', [longitude, latitude]); // conversion from (long/lat) to UTM (E/N)
+        let utmEasting = Number(en_m[0].toFixed(4));
+        let utmNorthing = Number(en_m[1].toFixed(4));
+        return ('UTM  Zone:' + utmZone + ' UTM Easting:' + utmEasting + ' UTM Northing:' + utmNorthing);
+}
+
+const Measure = ({ map } : { map: any }) => {
+  const classes = useStyles();
+  const [position, setPosition] = useState(map.getCenter());
+  const [measureMode, setMeasureMode] = useState(false);
+
+  const handleClick = (event: any) => {
+    event.preventDefault();
+    setMeasureMode(!measureMode);
+  };
+  //console.log(measureMode);
+  //console.log(position);
+
+  map = useMapEvents({
+    click(e) {
+      console.log(e.latlng);
+      setPosition(e.latlng);
+    } 
+  });
+
+  const icon = L.icon({ iconUrl: dotMarker, iconSize: [16,16] });
+
+	return (
+		<div style={{zIndex: 500}}>
+			<IconButton className={classes.rulerButton} onClick={handleClick}>
+				<img className={classes.image} src={ruler} />
+      </IconButton>
+      { measureMode ? 
+        <Marker position={position} icon={icon} >
+          <Popup>{utm_zone(position.lng, position.lat)}</Popup>
+        </Marker> : null
+      }
+		</div>
+	);
+};
+
+export default Measure;
